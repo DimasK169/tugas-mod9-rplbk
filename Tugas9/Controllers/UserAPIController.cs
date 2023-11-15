f118c5af801ab1aa6077575914472d7f9b07a3e3
Dimas Ardani <dimaskenang@students.undip.ac.id>
2023-11-15 16:02:49 +0700

    ain

--- Tugas9/Controllers/UserAPIController.cs
+++ Tugas9/Controllers/UserAPIController.cs
@@ -1,17 +1,98 @@
 ﻿namespace Tugas9.Controllers;
 using Models.Dto;
 using Microsoft.AspNetCore.Mvc;
+using Tugas9.Data;
+using Microsoft.AspNetCore.JsonPatch;
+
 [Route("api/UserAPI")]
 [ApiController]
 public class UserAPIController : ControllerBase
 {
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     [HttpGet]
-    public IEnumerable<UserDTO> GetUsers()
+    public ActionResult<IEnumerable<UserDTO>> GetUser()
     {
-        return new List<UserDTO> {
-            new UserDTO{Id=1, Name="Dimas", Password="123"},
-            new UserDTO{Id=2,Name="Kenang", Password="123"}
-            };
+        return UserStore.userList;
+    }
+    [HttpGet("{id:int}", Name = "GetUser")]
+    public ActionResult<UserDTO> GetUser(int id)
+    {
+        if (id == 0) return BadRequest();
+        var user = UserStore.userList.FirstOrDefault(u => u.Id == id);
+        if (user == null) return NotFound();
+        return Ok(user);
+    }
+    [HttpPost]
+    public ActionResult<UserDTO> CreateUser([FromBody] UserDTO userDTO)
+    {
+        if (UserStore.userList.FirstOrDefault(u => u.Name.ToLower() == userDTO.Name.ToLower()) != null)
+        {
+            ModelState.AddModelError("CustomError", "User already exist");
+            return BadRequest(ModelState);
+        }
+        if (userDTO == null)
+        {
+            return BadRequest(userDTO);
+        }
+        if (userDTO.Id == 0)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+        userDTO.Id = UserStore.userList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+        UserStore.userList.Add(userDTO);
+        string response = "Sukses menambahkan data user" + "\nId : " + userDTO.Id.ToString() + "\nNama : " + userDTO.Name + "\nPassword" + userDTO.Password;
+
+        return CreatedAtRoute("GetVilla", new { id = userDTO.Id }, response);
+
+    }
+    [HttpDelete("(id:int)", Name = "DeleteUser")]
+    public IActionResult DeleteUser(int id)
+    {
+        if (id == 0) 
+        {
+            return BadRequest();
+        }
+        var user = UserStore.userList.FirstOrDefault(u => u.Id == id);
+        if (user == null) 
+        {
+            return NotFound();
+        }
+        UserStore.userList.Remove(user);
+        return NoContent();
+    }
+    [HttpPut("(id:int", Name = "Update User")]
+    public IActionResult UpdateUser(int id, [FromBody]UserDTO userDTO)
+    {
+        if (userDTO == null || id != userDTO.Id) 
+        {
+            return BadRequest();
+        }
+        var user = UserStore.userList.FirstOrDefault( u => u.Id == id);
+        user.Name = userDTO.Name;
+        user.Password = userDTO.Password;
+
+        return NoContent();
+    }
+    [HttpPatch("{id:int}", Name = "UpdatePartialUser")]
+    public IActionResult UpdatePartialUser (int id, JsonPatchDocument<UserDTO> patchDTO)
+    {
+        if (patchDTO == null || id == 0)
+        {
+            return BadRequest();
+        }
+        var user = UserStore.userList.FirstOrDefault(u => u.Id == id);
+        if (user == null)
+        {
+            return BadRequest();
+        }
+        patchDTO.ApplyTo(user, ModelState);
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        return NoContent();
     }
 }
     
---
+++ Tugas9/Data/UserStore.cs
@@ -0,0 +1,13 @@
+﻿namespace Tugas9.Data;
+using Models.Dto;
+using Microsoft.AspNetCore.Identity;
+
+    public static class UserStore
+    {
+    public static List<UserDTO> userList = new List<UserDTO>
+        {
+            new UserDTO {Id =  1, Name = "Dimas", Password = "123"},
+            new UserDTO {Id = 2, Name = "Kenang", Password = "321"}
+        };
+    }
+
